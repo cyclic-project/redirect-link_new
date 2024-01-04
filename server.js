e1a68f856735cd205aca3618900a46240fb35d61
marselk12 <124248898+marselk12@users.noreply.github.com>
2024-01-04 11:04:36 +0700

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.post('/adddata', async (req, res) => {
 
   try {
     // Baca file JSON yang telah disimpan di server 2
-    const data = await fs.readFile('data_server2.json', 'utf-8');
+    const data = await readData('data_server2.json');
     let jsonData = JSON.parse(data);
 
     // Tambahkan data yang diterima dari server 1 ke dalam array yang sudah ada
@@ -44,7 +44,8 @@ app.post('/writedata', async (req, res) => {
 
  // Simpan data dalam bentuk file di server 2
  try {
-  await fs.writeFile('data_server2.json', JSON.stringify(dataToSave));
+  await saveToJsonFile(dataToSave, 'data_server2.json');
+  // await fs.writeFile('data_server2.json', JSON.stringify(dataToSave));
   console.log('Data berhasil disimpan di Server 2');
   res.send('Data berhasil diterima dan disimpan di Server 2');
  } catch (error) {
@@ -58,14 +59,14 @@ app.post('/deletedata', async (req, res) => {
 
   try {
     // Baca file JSON yang telah disimpan di server 2
-    const data = await fs.readFile('data_server2.json', 'utf-8');
+    const data = await readData('data_server2.json');
     let jsonData = JSON.parse(data);
 
     // Filter data berdasarkan path yang akan dihapus
     jsonData = jsonData.filter(item => item.path !== dataToDelete.path);
 
     // Simpan data yang telah dihapus kembali ke dalam file JSON
-    await fs.writeFile('data_server2.json', JSON.stringify(jsonData, null, 2));
+    await saveToJsonFile(jsonData, 'data_server2.json');
 
     console.log(`Data dengan path ${dataToDelete.path} berhasil dihapus di Server 2`);
     res.send(`Data dengan path ${dataToDelete.path} berhasil dihapus di Server 2`);
@@ -78,7 +79,7 @@ app.post('/deletedata', async (req, res) => {
 const updateData = async (newPath, newUrl, updatePath, footerBoleaan) => {
  try {
   // Baca data dari file
-  const data = await fs.readFile('data_server2.json', 'utf-8');
+  const data = await readData('data_server2.json');
 
   const dataArray = JSON.parse(data);
 
@@ -98,7 +99,7 @@ const updateData = async (newPath, newUrl, updatePath, footerBoleaan) => {
   }
 
   // Simpan data yang diperbarui ke file
-  await fs.writeFile('data_server2.json', JSON.stringify(dataArray, null, 2));
+  await saveToJsonFile(dataArray, 'data_server2.json');
  } catch (error) {
   console.error(error);
   throw new Error(`Failed to update data. ${error.message}`);
@@ -112,7 +113,7 @@ app.put('/updateData/:path', async (req, res) => {
  // Update data menggunakan fungsi yang telah dibuat
  try {
   // Lakukan validasi di sini sebelum memanggil fungsi updateData
-  const data = await fs.readFile('data_server2.json', 'utf-8');
+  const data = await readData('data_server2.json');
   const dataArray = JSON.parse(data);
   
   let blockIt;
@@ -175,7 +176,7 @@ app.put('/updateData/:path', async (req, res) => {
 app.get('/getdata', async (req, res) => {
  // Baca file JSON yang telah disimpan di server 2
  try {
-  const data = await fs.readFile('data_server2.json', 'utf-8');
+  const data = await readData('data_server2.json');
   const jsonData = JSON.parse(data);
   console.log('Data berhasil dibaca di Server 2:', jsonData);
   res.json(jsonData);
@@ -185,7 +186,7 @@ app.get('/getdata', async (req, res) => {
  }
 });
 
-// DOWNLOADER
+// DOWNLOADER SOCIAL MEDIA VIDEO
 app.get('/download/:platform/:formatConvert/:formatOriginal/:indexData', async (req, res) => {
  const { formatConvert, formatOriginal, indexData } = req.params
  const { urlVideo } = req.query
@@ -265,6 +266,185 @@ app.get('/download/:platform/:formatConvert/:formatOriginal/:indexData', async (
   }
  }
 })
+
+app.post('/getFullData/', async (req, res) => {
+ const jsonData = await readData("limits_download.json");
+ (jsonData) ? res.status(201).json(jsonData) : res.status(202).json({ message: "Data not found" })
+})
+
+app.post('/manageLimit', async (req, res) => {
+ const { platform, configData } = req.body;
+ const option = configData.option;
+ const code = configData.code;
+ var message;
+
+ try {
+  const jsonData = await readData('limits_download.json')
+  const data = JSON.parse(jsonData);
+
+  const codeObject = data.find(item => item.platform === platform && item.code === code);
+
+  if (codeObject) {
+   if (option == "reset") {
+    codeObject.limit = 0;
+    message = "Reset to 0 (Limit) Successfully"
+   }
+   if (option == "unlimited") {
+    codeObject.unlimited = (codeObject.unlimited == 1) ? 0 : 1;
+    message = `Set to ${codeObject.unlimited} Unlimited Successfully`
+   }
+   if (option == "nama") {
+    codeObject.name = configData.nama
+    message = `Set nama Successfully`
+   }
+   if (option == "set") {
+    codeObject.maxLimit = (configData.maxLimit) ? parseInt(configData.maxLimit) : codeObject.maxLimit;
+    message = `Set MaxLimit to ${codeObject.maxLimit} Successfully`
+   }
+   await saveToJsonFile(data, 'limits_download.json')
+
+   res.status(200).json({ message });
+  } else {
+   res.status(404).send('Code not found.');
+  }
+ } catch (error) {
+  console.error('Error:', error.message);
+  res.status(500).send('Internal Server Error');
+ }
+});
+
+function isSameDate(dateString1, dateString2) {
+ const date1 = new Date(dateString1);
+ const date2 = new Date(dateString2);
+
+ return (
+  date1.getFullYear() === date2.getFullYear() &&
+  date1.getMonth() === date2.getMonth() &&
+  date1.getDate() === date2.getDate()
+ );
+}
+
+app.post('/getLimit/:platform', async (req, res) => {
+ const { code, currentDate } = req.body;
+ const { platform } = req.params;
+
+ try {
+  // Baca isi file JSON
+  const jsonContent = await readData('limits_download.json')
+  const data = JSON.parse(jsonContent);
+
+  const foundItem = data.find(item => item.platform === platform && item.code === code);
+
+  if (foundItem) {
+   if (isSameDate(currentDate, foundItem.date)) {
+    foundItem.remainLimits = foundItem.maxLimit - foundItem.limit
+    res.json({ limit: foundItem.limit, remainLimits: foundItem.remainLimits, unlimitedd: foundItem.unlimited });
+   } else {
+    foundItem.limit = 0;
+    foundItem.date = currentDate;
+
+    res.json({ limit: foundItem.limit });
+   }
+   saveToJsonFile(data, 'limits_download.json');
+  } else {
+   res.status(404).json({ error: 'Code not found' });
+  }
+ } catch (error) {
+  console.error('Error reading JSON file:', error.message);
+  res.status(500).json({ error: 'Internal Server Error' });
+ }
+});
+
+app.post('/updateCodeLimit/:platform', async (req, res) => {
+ try {
+  const requestData = req.body;
+
+  const jsonData = await readData("limits_download.json")
+  const data = JSON.parse(jsonData);
+
+  const codeObject = data.find(item => item.platform === req.params.platform && item.code === requestData.code);
+  const currentLimit = parseInt(codeObject.limit) || 0;
+
+  if (codeObject) {
+   const newLimit = currentLimit + 1;
+
+   if (codeObject.unlimited == "1") {
+    codeObject.limit = 0;
+    saveToJsonFile(data, 'limits_download.json')
+    res.status(200).json({ success: true, unlimited: 1 })
+   } else {
+    if (newLimit <= 10) {
+     codeObject.limit = newLimit;
+     codeObject.remainLimits = codeObject.maxLimit - newLimit
+
+     saveToJsonFile(data, 'limits_download.json');
+
+     res.status(200).json({ success: true, limit: newLimit, maxLimit: codeObject.maxLimit });
+    } else {
+     // res.status(304).json({ error: 'Limit sudah mencapai batas maksimum' });
+     res.status(203).json({ message: 'limit sudah tercapai!', limit: currentLimit })
+    }
+   }
+  } else {
+   res.status(404).send('Code tidak ditemukan');
+  }
+ } catch (error) {
+  console.error('Terjadi kesalahan:', error.message);
+  res.status(500).send('Terjadi kesalahan');
+ }
+});
+
+function codeExists(code, platform, jsonData) {
+ return jsonData.some(item => item.code === code && item.platform === platform);
+}
+
+app.post('/generate_code/:platform', async (req, res) => {
+ let jsonData = [];
+ const limitsData = await readData('limits_download.json')
+ const dataLimits = JSON.parse(limitsData);
+ const randomCode = generateRandomCode();
+ const currentDate = new Date();
+ const formattedDate = `${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()}`;
+
+ while (codeExists(randomCode, req.params.platform, dataLimits)) {
+  randomCode = generateRandomCode();
+ }
+
+ jsonData.push({
+  "platform": req.params.platform,
+  "code": randomCode,
+  "limit": 0,
+  "date": formattedDate,
+  "maxLimit": 10,
+  "name": "",
+  "unlimited": 0,
+  "remainLimits": 10,
+ });
+
+ saveToJsonFile(jsonData, 'limits_download.json');
+
+ res.json({ code: randomCode });
+});
+
+function generateRandomCode() {
+ const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+ let randomCode = '';
+ for (let i = 0; i < 30; i++) {
+  randomCode += characters.charAt(Math.floor(Math.random() * characters.length));
+ }
+ return randomCode;
+}
+
+// SAVE CODE UNIVERSAL
+function saveToJsonFile(data, fileName) {
+ fs.writeFile(fileName, JSON.stringify(data, null, 2), 'utf-8');
+ // fs.writeFileSync('limits_download.json', JSON.stringify(data, null, 2), 'utf-8');
+}
+
+async function readData(fileName) {
+ const data = await fs.readFile(fileName, 'utf-8') || "[]";
+ return data;
+}
 
 app.listen(port, host, () => {
  console.log(`Server 2 berjalan di http://localhost:${port}`);
